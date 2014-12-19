3461021d6fb2f1d2d0fdbc72eafe3ca465e5a68a
Madan Mohan Venkat Baggu <madanvenkat535@gmail.com>
2014-12-19 17:34:50 +0530

    recaptcha publish
    recaptcha publish

---
+++ GooglereCAPTCHa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GooglereCAPTCHa.Startup))]
+namespace GooglereCAPTCHa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
